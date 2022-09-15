7e4c9cd42347c368b43f529633372de191bae61e
David Craig <37514553+dcraig327@users.noreply.github.com>
2022-09-15 01:21:27 -0700

    alternal FizzBuzz implementation

--- javascript.js
+++ javascript.js
@@ -22,16 +22,23 @@ function main() {
   let number = getNumber();
   //log("Starting number: " + number);
 
-  for (let i = 1; i <= number; i++) {
-    if (i % 15 == 0) {
-      log("FizzBuzz");
-    } else if (i % 3 == 0) {
-      log("Fizz");
-    } else if (i % 5 == 0) {
-      log("Buzz");
-    } else {
-      log(i);
+  if (number >= 1) {
+    log("FizzBuzz");
+  }
+  for (let i = 2; i <= number; i++) {
+    let fizzbuzz = "";
+
+    if (i % 3 == 0) {
+      fizzbuzz += "Fizz";
     }
+    if (i % 5 == 0) {
+      fizzbuzz += "Buzz";
+    }
+    if (!fizzbuzz) {
+      fizzbuzz = i.toString();
+    }
+
+    log(fizzbuzz);
   }
 }
 
