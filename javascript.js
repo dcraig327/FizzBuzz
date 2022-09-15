84523f3d56e450be63fe4a4bb5e5b36c827051e3
David Craig <37514553+dcraig327@users.noreply.github.com>
2022-09-15 00:44:03 -0700

    get user input
    created index.html & javascript.js and able to get user input

---
+++ javascript.js
@@ -0,0 +1,7 @@
+// enter a number
+// loop and print from 1 to the number entered
+// %3 print Fizz instead of the number
+// %5 print Buzz instead of the number
+// %3 and %5 print FizzBuzz
+let number = parseInt(prompt("Enter a FizzBuzz number."));
+console.log(number);
